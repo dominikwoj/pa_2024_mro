cc4b9eeed7f7d2c5f911aa7a881a8a572672ae87
Dominik Wojnowski <dwojnow1@intercars.eu>
2024-03-06 22:33:35 +0100

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+> https://sio2.mimuw.edu.pl/c/pa-2024-1/p/mro/
+>Zadanie: MRO
+Mrówki
+Potyczki Algorytmiczne 2024, runda próbna. Limity: 1024 MB, 1 s. 05.03.2024
---
+++ mro.py
@@ -0,0 +1,28 @@
+def fun(n, state):
+    #    print(n, state)
+    ant_state = list(map(str, state))
+    ant_bite = [0] * n
+    ant_value = []
+    for i in range(n):
+        ant_value.append(float(i))
+
+    # print(ant_state, ant_value, ant_bite)
+    ant_step = 0.5
+
+    for i in range(n):
+        for j in range(n):
+            ant_value[j] += -ant_step if ant_state[j] == 'L' else ant_step
+        for k in range(n - 1):
+            if ant_value[k] == ant_value[k + 1]:
+                for l in [k, k + 1]:
+                    ant_state[l] = 'P' if ant_state[l] == 'L' else 'L'
+                    ant_bite[l] += 1
+
+    print(' '.join(list(map(str, ant_bite))))
+
+
+if __name__ == '__main__':
+    # fun(6, 'LPPLPL')
+    n = int(input())
+    state = input()
+    fun(n, state)
